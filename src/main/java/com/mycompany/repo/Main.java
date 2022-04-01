174590381a0c12f3880c27068e90e282cd35d0bd
Lautaro Vallejos <lautaro.vallejosk4m@gmail.com>
2022-03-31 21:28:33 -0300

    Serie de ejercicios con vectores completada

--- src/main/java/com/mycompany/repo/Main.java
+++ src/main/java/com/mycompany/repo/Main.java
@@ -6,27 +6,55 @@ import java.util.Scanner;
 /*Asunto: Vectores
     Ejercicio 1: Cargar un vector | X
     Ejercicio 2: Mostrar el vector | X
-    Ejercicio 3: Mostrar el numero mayor | sin completar
-    Ejercicio 4: Mostrar el menor | sin completar
-    Ejercicio 5: Mostrar la suma de todos los numeros | sin completar
-    Ejercicio 6: Mostrar el promedio de todos los numeros | sin completar 
+    Ejercicio 3: Mostrar el numero mayor | X
+    Ejercicio 4: Mostrar el menor | X
+    Ejercicio 5: Mostrar la suma de todos los numeros | X
+    Ejercicio 6: Mostrar el promedio de todos los numeros | X
 */
 public class Main {
     public static void main(String[] args) {
         //Declaraciones
-        int num[], cant = 9, i;
+        int num[], cant = 4, i, mayor, menor, suma = 0;
+        float prom;
         Scanner input = new Scanner(System.in);
         num = new int[cant];
         
-        //Cargar el vector
+        //Cargar el vector 
         for (i = 0; i < cant; i++) {
             System.out.println("Ingrese numero posicion " + (i+1));
             num[i] = input.nextInt();
+            suma += num[i];
         }
+        
+        //Se setea el valor incial para compararlos
+        mayor = num[0];
+        menor = num[0];
+        
+        //Se recorre nuevamente y se comparan
+        for (i = 0; i < cant; i++) {
+            if(num[i] > mayor){
+                mayor = num[i];
+            }
+            
+            if(num[i] < menor){
+                menor = num[i];
+            }
+        }
+        prom = suma/cant;
+        System.out.println("Los numeros ingresados en el vector son: ");
         
         //Mostrar numeros cargados en el vector
         for(i=0; i<cant ; i++){
             System.out.println(num[i]);
         }
+        
+        //Mostrar el mayor
+        System.out.println("El mayor numero del vector es: " + mayor);
+        //Mostrar el menor
+        System.out.println("El menor numero del vector es: " + menor);
+        //Muestra la suma
+        System.out.println("La suma de todos los numeros son: " + suma);
+        //Muesta el promedio
+        System.out.println("El promedio de todos los valores del vector es: " + prom);
     }
 }
