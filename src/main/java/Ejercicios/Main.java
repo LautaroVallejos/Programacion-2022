8db762ed7c30f4c3668392969994c0544fca8c0f
Lautaro Vallejos <lautaro.vallejosk4m@gmail.com>
2022-04-28 21:05:29 -0300

    Ejercicio 2 - Sobrecarga de Metodos

--- src/main/java/Ejercicios/Main.java
+++ src/main/java/Ejercicios/Main.java
@@ -3,17 +3,28 @@ package Ejercicios;
 /**
  *
  * @author Coffee Lover
- * Ejercicio 1 - Sobrecarga de Métodos
+ * Ejercicio 2 - Sobrecarga de Métodos
  *  
- * Realizar la clase Promedio que tiene cuatro atributos enteros (num1,
-    num2, num3 y num4) su constructor que inicializa todos los atributos y el
-    método mostrarPromedio() el cuál calcula el promedio de los atributos y
-    los muestra. En la clase Main tiene el método main que declara un
-    objeto de la clase Promedio y llama al método mostrarPromedio().
+ * Rehacer el ejercicio número 1 con las siguientes modificaciones:
+    Clase promedio se le agrega el atributo prom, no tiene constructor, tiene
+    el método calcularPromedio() con sobrecarga, a uno no le paso nada y
+    se encarga de pedir los 4 números y al otro le debo pasar (mediante
+    argumentos) los 4 números, este método debe calcular el promedio y
+    guardarlo en el atributo prom y el método mostrarPromedio() que se
+    encarga de mostrar el contenido del atributo prom.
+    Clase Main se declara un objeto de la clase Promedio, se llama al
+    método calcularPromedio() pasándole los 4 argumentos y al método
+    mostrarPromedio(), luego se llama nuevamente al método
+    calcularPromedio() sin pasarle los argumento y por último al método
+    mostrarPromedio()
  */
+
 public class Main {
     public static void main(String[] args) {
         Promedio firstProm = new Promedio();
-        firstProm.mostarPromedio(20, 500, 150, 10);
+        firstProm.calcularPromedio(20, 20, 20, 20);
+        firstProm.mostarPromedio();
+        firstProm.calcularPromedio();
+        firstProm.mostarPromedio();
     }
 }
--- src/main/java/Ejercicios/Promedio.java
+++ src/main/java/Ejercicios/Promedio.java
@@ -1,37 +1,59 @@
 package Ejercicios;
-
+import java.util.Scanner;
 /**
  *
  * @author Coffee Lover
- * Ejercicio 1 - Sobrecarga de Métodos
+ * Ejercicio 2 - Sobrecarga de Métodos
  *  
- * Realizar la clase Promedio que tiene cuatro atributos enteros (num1,
-    num2, num3 y num4) su constructor que inicializa todos los atributos y el
-    método mostrarPromedio() el cuál calcula el promedio de los atributos y
-    los muestra. En la clase Main tiene el método main que declara un
-    objeto de la clase Promedio y llama al método mostrarPromedio().
+ * Rehacer el ejercicio número 1 con las siguientes modificaciones:
+    Clase promedio se le agrega el atributo prom, no tiene constructor, tiene
+    el método calcularPromedio() con sobrecarga, a uno no le paso nada y
+    se encarga de pedir los 4 números y al otro le debo pasar (mediante
+    argumentos) los 4 números, este método debe calcular el promedio y
+    guardarlo en el atributo prom y el método mostrarPromedio() que se
+    encarga de mostrar el contenido del atributo prom.
+    Clase Main se declara un objeto de la clase Promedio, se llama al
+    método calcularPromedio() pasándole los 4 argumentos y al método
+    mostrarPromedio(), luego se llama nuevamente al método
+    calcularPromedio() sin pasarle los argumento y por último al método
+    mostrarPromedio()
  */
 public class Promedio {
-    int num1, num2, num3, num4;
+    int num1, num2, num3, num4, prom;
     
-    //Metodo contructor
-    public Promedio(){
-        this.num1 = num1;
-        this.num2 = num2;
-        this.num3 = num3;
-        this.num4 = num4;
+//    //Metodo contructor
+//    public Promedio(){
+//        this.num1 = num1;
+//        this.num2 = num2;
+//        this.num3 = num3;
+//        this.num4 = num4;
+//        this.prom = prom;
+//    }
+   
+    public void calcularPromedio(){
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Ingrese primer numero: ");
+        num1 = input.nextInt();
+        System.out.println("\nIngrese segundo numero: ");
+        num2 = input.nextInt();
+        System.out.println("\nIngrese tercer numero: ");
+        num3 = input.nextInt();
+        System.out.println("\nIngrese cuarto numero: ");
+        num4 = input.nextInt();
     }
     
-    public void mostarPromedio(int num1, int num2, int num3, int num4){
-        int total = 0;
+    public void calcularPromedio(int num1, int num2, int num3, int num4){
+        prom=0;
+        prom+=num1;
+        prom+=num2;
+        prom+=num3;
+        prom+=num4;
         
-        total+=num1;
-        total+=num2;
-        total+=num3;
-        total+=num4;
-        
-        total = total/4;
-        
-        System.out.println("El promedio de los numeros dados es de: " + total);
+        prom = prom / 4;
+    }
+ 
+    public void mostarPromedio(){
+        System.out.println("El promedio de los numeros dados es de: " + prom);
     }
 }
