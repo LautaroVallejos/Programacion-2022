c578c418f6ce4f75f492abd3eab852d64764538a
Lautaro Vallejos <lautaro.vallejosk4m@gmail.com>
2022-04-28 22:10:34 -0300

    Ejercicio 3 - Sobrecarga de Metodos

--- src/main/java/Calculadora/Calculadora.java
+++ src/main/java/Calculadora/Calculadora.java
@@ -1,6 +1,7 @@
 //Recursos
 package Calculadora;
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 /**
  * @author Coffee Lover
@@ -11,40 +12,102 @@ public class Calculadora {
     static Scanner input = new Scanner(System.in);
     public static float numA, numB, res;
     
-//Ingreso de Numeros
-    public static float setNumber1(){
-        System.out.println("Ingrese primer numero");
-        numA = input.nextFloat();
-        return numA;
+    public void Calculadora(){
+        this.numA = numA;
+        this.numB = numB;
+        this.res = res;
     }
     
-    public static float setNumber2(){
-        System.out.println("Ingrese segundo numero");
-        numB = input.nextFloat();
-        return numB;
-    }
+//Ingreso de Numeros
+//    public static float setNumber1(){
+//        System.out.println("Ingrese primer numero");
+//        numA = input.nextFloat();
+//        return numA;
+//    }
+//    
+//    public static float setNumber2(){
+//        System.out.println("Ingrese segundo numero");
+//        numB = input.nextFloat();
+//        return numB;
+//    }
 
 //Funcion de Suma
-    public static float sumar(float numA,float numB){
+    public static void sumar(float numA,float numB){
         res = numA + numB;
-        return res;
+        System.out.println("El resultado de la suma es: " + res);
+    }
+    
+    public static void sumar(){
+        int op = JOptionPane.showConfirmDialog(null, "Desea ingresar numeros?");
+        if(op == JOptionPane.YES_OPTION){
+            System.out.println("Ingrese primer numero");
+            numA = input.nextFloat();
+            System.out.println("Ingrese segundo numero");
+            numB = input.nextFloat();
+            res = numA + numB;
+            System.out.println("El resultado de la suma es: " + res);
+        } else{
+            System.out.println("Bueno... mi intencion no era ofenderte.\nReintenta cuando quieras.");
+        }
     }
 
 //Funcion de Resta
-    public static float restar(float numA,float numB){
+    public void restar(float numA,float numB){
         res = numA - numB;
-        return res;
+        System.out.println("El resultado de la resta es: " + res);
+    }
+    
+    public void restar(){
+        int op = JOptionPane.showConfirmDialog(null, "Desea ingresar numeros?");
+        if(op == JOptionPane.YES_OPTION){
+            System.out.println("Ingrese primer numero");
+            numA = input.nextFloat();
+            System.out.println("Ingrese segundo numero");
+            numB = input.nextFloat();
+            res = numA - numB;
+            System.out.println("El resultado de la resta es: " + res);
+        } else{
+            System.out.println("Bueno... mi intencion no era ofenderte.\nReintenta cuando quieras.");
+        }
     }
     
 //Funcion de Multiplicacion
-    public static float multiplicar(float numA,float numB){
+    public void multiplicar(float numA, float numB){
         res = numA * numB;
-        return res;
+        System.out.println("El resultado de la multiplicacion es: " + res);
+    }
+    
+    public void multiplicar(){
+        int op = JOptionPane.showConfirmDialog(null, "Desea ingresar numeros?");
+        if(op == JOptionPane.YES_OPTION){
+            System.out.println("Ingrese primer numero");
+            numA = input.nextFloat();
+            System.out.println("Ingrese segundo numero");
+            numB = input.nextFloat();
+            res = numA * numB;
+            System.out.println("El resultado de la multiplicacion es: " + res);
+        } else{
+            System.out.println("Bueno... mi intencion no era ofenderte.\nReintenta cuando quieras.");
+        }
     }
     
 //Funcion de Division
-    public static float dividir(float numA,float numB){
+    public void dividir(float numA,float numB){
         res = numA / numB;
-        return res;
+        System.out.println("El resultado de la division es: " + res);
+    }
+    
+    public void dividir(){
+        int op = JOptionPane.showConfirmDialog(null, "Desea ingresar numeros?");
+        if(op == JOptionPane.YES_OPTION){
+            System.out.println("Ingrese primer numero");
+            numA = input.nextFloat();
+            System.out.println("Ingrese segundo numero");
+            numB = input.nextFloat();
+            res = numA / numB;
+            System.out.println("El resultado de la division es: " + res);
+        } else{
+            System.out.println("Bueno... mi intencion no era ofenderte.\nReintenta cuando quieras.");
+        }
     }
 }
--- src/main/java/Calculadora/Main.java
+++ src/main/java/Calculadora/Main.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 
 public class Main {
     static Scanner input = new Scanner(System.in);
+    static Calculadora operacion = new Calculadora();
     public static float numA, numB;
        
     public static void main(String[] args) {
@@ -16,20 +17,16 @@ public class Main {
         //Inicio del programa
         System.out.println("Inicio de la calculadora... \n");
         
-        //Ingreso de los numeros  
-        numA = Calculadora.setNumber1();
-        numB = Calculadora.setNumber2();
-        
         //Ingrso de la operacion
         System.out.println("Ingresar Operacion: \n1. Sumar\n2. Restar\n3. Multiplicar\n4. Dividir");
         op = input.nextInt();
         
         //Logica
         switch(op){
-            case 1: System.out.println("El resultado de la suma es: " + Calculadora.sumar(numA, numB));break;
-            case 2: System.out.println("El resultado de la resta es: " + Calculadora.restar(numA, numB));break;
-            case 3: System.out.println("El resultado de la multiplicacion es: " + Calculadora.multiplicar(numA, numB));break;
-            case 4: System.out.println("El resultado de la division es: " + Calculadora.dividir(numA, numB));break;
+            case 1: operacion.sumar(2, 45); operacion.sumar();break;
+            case 2: operacion.restar(50, 20); operacion.restar();break;
+            case 3: operacion.multiplicar(2, 10); operacion.multiplicar();break;
+            case 4: operacion.dividir(30, 5); operacion.dividir();break;
             default: System.out.println("Error en el ingreso de la operacion, intente nuevamente");break;
         }
     }
